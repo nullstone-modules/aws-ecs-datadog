836817cf15325303daf16607298f9be1959a413d
Brad Sickles <brad.sickles@gmail.com>
2021-09-29 17:35:40 -0400

    Initial draft

---
+++ nullstone.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    ns = {
+      source = "nullstone-io/ns"
+    }
+  }
+}
+
+data "ns_workspace" "this" {}
+
+variable "app_metadata" {
+  description = <<EOF
+App Metadata is injected from the app on-the-fly.
+This contains information about resources created in the app module that are needed by the capability.
+EOF
+
+  type    = map(string)
+  default = {}
+}
---
+++ outputs.tf
@@ -0,0 +1,22 @@
+output "log_configurations" {
+  value = [
+    {
+      logDriver = "awsfirelens"
+      options = jsonencode({
+        "Name" : "datadog",
+        "Host" : "http-intake.logs.datadoghq.com",
+        "TLS" : "on",
+        "dd_service" : data.ns_workspace.this.block_name,
+        "dd_source" : "httpd",
+        "dd_tags" : "stack:${data.ns_workspace.this.stack_name}, env:${data.ns_workspace.this.env_name}",
+        "provider" : "ecs"
+      })
+      secretOptions = jsonencode([
+        {
+          name : "apikey",
+          valueFrom : local.api_key
+        }
+      ])
+    }
+  ]
+}
