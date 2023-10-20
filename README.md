0f4914a2a41b0165a22edb67e78fb6d769550576
Brad Sickles <brad.sickles@gmail.com>
2023-10-20 10:33:07 -0400

    wip

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# aws-datadog-fargate
-Capability to report logs and metrics for an AWS fargate service to Datadog
+# Datadog Logs/Metrics for ECS/Fargate
+
+This capability sends application logs and metrics to Datadog for ECS (Fargate-based or EC2-based).
+
+## Logs
+
+Application logs are configured to send to Datadog in near real-time. (<1 min latency)
+These logs are tagged with `stack`, `block`, and `env`.
+
+The application logs are immediately sent to Cloudwatch and transmitted to Datadog via Kinesis Firehose.
+
+## Metrics
+
+The Datadog agent is added to your application as a sidecar container.
+This agent collects metrics from AWS and custom metrics from your application and sends them to Datadog in near real-time.
---
+++ agent.tf
@@ -0,0 +1,41 @@
+output "sidecars" {
+  value = [
+    {
+      name         = "datadog-agent"
+      image        = "public.ecr.aws/datadog/agent:latest"
+      essential    = true
+      portMappings = [
+        { protocol = "tcp", containerPort = 8126 },
+      ]
+      environment  = [
+        { name = "ECS_FARGATE", value = "true" },
+        { name = "DD_APM_ENABLED", value = "true" },
+        { name = "DD_SITE", value = "datadoghq.com" }
+      ]
+      secrets = [{ name = "DD_API_KEY", valueFrom = local.api_key_secret_id }]
+    }
+  ]
+}
+
+resource "aws_iam_role_policy_attachment" "execution-datadog" {
+  role       = local.execution_role_name
+  policy_arn = aws_iam_policy.datadog.arn
+}
+
+resource "aws_iam_policy" "datadog" {
+  name   = "${local.resource_name}-datadog"
+  policy = data.aws_iam_policy_document.datadog.json
+}
+
+data "aws_iam_policy_document" "datadog" {
+  statement {
+    sid       = "AllowReadDatadogApiKey"
+    effect    = "Allow"
+    resources = [local.api_key_secret_id]
+
+    actions = [
+      "secretsmanager:GetSecretValue",
+      "kms:Decrypt"
+    ]
+  }
+}
