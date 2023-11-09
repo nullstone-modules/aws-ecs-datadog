33bb27e85a4241b0851ffa2e547d2ce34ced1fd6
Brad Sickles <brad.sickles@gmail.com>
2023-11-08 20:58:31 -0500

    Configure/Export open telemetry

--- agent.tf
+++ agent.tf
@@ -1,20 +1,23 @@
-output "sidecars" {
-  value = [
-    {
-      name         = "datadog-agent"
-      image        = "public.ecr.aws/datadog/agent:latest"
-      essential    = true
-      portMappings = jsonencode([
-        { protocol = "tcp", containerPort = 8126 },
-      ])
-      environment  = jsonencode([
-        { name = "ECS_FARGATE", value = "true" },
-        { name = "DD_APM_ENABLED", value = "true" },
-        { name = "DD_SITE", value = "datadoghq.com" }
-      ])
-      secrets = jsonencode([{ name = "DD_API_KEY", valueFrom = local.api_key_secret_id }])
-    }
-  ]
+locals {
+  sidecar_name = "datadog-agent"
+  agent_sidecar = {
+    name      = "datadog-agent"
+    image     = "public.ecr.aws/datadog/agent:latest"
+    essential = true
+    portMappings = jsonencode([
+      { protocol = "tcp", containerPort = 4317 },
+      { protocol = "tcp", containerPort = 4318 },
+      { protocol = "tcp", containerPort = 8126 },
+    ])
+    environment = jsonencode([
+      { name = "ECS_FARGATE", value = "true" },
+      { name = "DD_APM_ENABLED", value = "true" },
+      { name = "DD_SITE", value = "datadoghq.com" },
+      { name = "DD_OTLP_CONFIG_RECEIVER_PROTOCOLS_GRPC_ENDPOINT", value = "0.0.0.0:4317" },
+      { name = "DD_OTLP_CONFIG_RECEIVER_PROTOCOLS_HTTP_ENDPOINT", value = "0.0.0.0:4318" }
+    ])
+    secrets = jsonencode([{ name = "DD_API_KEY", valueFrom = local.api_key_secret_id }])
+  }
 }
 
 resource "aws_iam_role_policy_attachment" "execution-datadog" {
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,14 @@
-output "noop" {
-  value = []
+output "env" {
+  value = [
+    {
+      name  = "OTEL_EXPORTER_OTLP_ENDPOINT"
+      value = "http://${local.sidecar_name}:4318"
+    }
+  ]
+}
+
+output "sidecars" {
+  value = [
+    local.agent_sidecar
+  ]
 }
